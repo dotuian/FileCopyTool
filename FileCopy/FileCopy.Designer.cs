936db0c4eb89bb77fde74267a10fe67dd5e672f6
dotuian <dotuian@outlook.com>
2015-10-02 16:56:51 +0900

    first version

---
+++ FileCopy/FileCopy.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace FileCopy
+{
+    partial class FileCopy
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.importToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txtFileList = new System.Windows.Forms.TextBox();
+            this.txtDestination = new System.Windows.Forms.TextBox();
+            this.txtSource = new System.Windows.Forms.TextBox();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.menuStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(823, 26);
+            this.menuStrip1.TabIndex = 5;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.importToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(40, 22);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // importToolStripMenuItem
+            // 
+            this.importToolStripMenuItem.Name = "importToolStripMenuItem";
+            this.importToolStripMenuItem.Size = new System.Drawing.Size(117, 22);
+            this.importToolStripMenuItem.Text = "Import";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(117, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Controls.Add(this.progressBar);
+            this.panel1.Controls.Add(this.txtResult);
+            this.panel1.Controls.Add(this.txtFileList);
+            this.panel1.Controls.Add(this.txtDestination);
+            this.panel1.Controls.Add(this.txtSource);
+            this.panel1.Controls.Add(this.lblResult);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Location = new System.Drawing.Point(0, 29);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(811, 542);
+            this.panel1.TabIndex = 6;
+            // 
+            // txtFileList
+            // 
+            this.txtFileList.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtFileList.Location = new System.Drawing.Point(95, 90);
+            this.txtFileList.Multiline = true;
+            this.txtFileList.Name = "txtFileList";
+            this.txtFileList.Size = new System.Drawing.Size(700, 178);
+            this.txtFileList.TabIndex = 11;
+            // 
+            // txtDestination
+            // 
+            this.txtDestination.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtDestination.Location = new System.Drawing.Point(95, 54);
+            this.txtDestination.Name = "txtDestination";
+            this.txtDestination.Size = new System.Drawing.Size(700, 19);
+            this.txtDestination.TabIndex = 10;
+            this.txtDestination.DoubleClick += new System.EventHandler(this.txtDestination_DoubleClick);
+            // 
+            // txtSource
+            // 
+            this.txtSource.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtSource.Location = new System.Drawing.Point(95, 19);
+            this.txtSource.Name = "txtSource";
+            this.txtSource.Size = new System.Drawing.Size(700, 19);
+            this.txtSource.TabIndex = 9;
+            this.txtSource.DoubleClick += new System.EventHandler(this.txtSource_DoubleClick);
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Font = new System.Drawing.Font("YaHei Consolas Hybrid", 9.75F);
+            this.lblResult.Location = new System.Drawing.Point(225, 300);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(0, 17);
+            this.lblResult.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("YaHei Consolas Hybrid", 9.75F);
+            this.label2.Location = new System.Drawing.Point(4, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 17);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Destination";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("YaHei Consolas Hybrid", 9.75F);
+            this.label1.Location = new System.Drawing.Point(39, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 17);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Source";
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Font = new System.Drawing.Font("YaHei Consolas Hybrid", 9.75F);
+            this.button1.Location = new System.Drawing.Point(94, 294);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(118, 27);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Execute";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.execute_Click);
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(95, 268);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(700, 19);
+            this.progressBar.TabIndex = 12;
+            // 
+            // txtResult
+            // 
+            this.txtResult.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtResult.Location = new System.Drawing.Point(95, 329);
+            this.txtResult.Multiline = true;
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(700, 192);
+            this.txtResult.TabIndex = 11;
+            // 
+            // FileCopy
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(823, 562);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "FileCopy";
+            this.Text = "FileCopy";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem importToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox txtFileList;
+        private System.Windows.Forms.TextBox txtDestination;
+        private System.Windows.Forms.TextBox txtSource;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.TextBox txtResult;
+    }
+}
+
---
+++ FileCopy/FileCopy.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FileCopy
+{
+    public partial class FileCopy : Form
+    {
+        public FileCopy()
+        {
+            InitializeComponent();
+            init();
+        }
+
+        private void init() {
+
+            //可输入多行
+            txtFileList.Multiline = true;
+            //垂直、水平滚动条
+            txtFileList.ScrollBars = ScrollBars.Both;
+            //禁止自动换行
+            txtFileList.WordWrap = false;
+
+            //结果表示
+            txtResult.Multiline = true;
+            txtResult.ScrollBars = ScrollBars.Both;
+            txtResult.WordWrap = false;
+            txtResult.ReadOnly = true;
+            txtResult.BackColor = System.Drawing.SystemColors.ControlLight;
+
+            // 进度条不显示
+            progressBar.Hide();
+
+
+            txtSource.Text = @"c:\";
+            txtDestination.Text = @"d:\";
+            txtFileList.Text = "1\r\n2\r\n2\r\n2\r\n2\r\n2\r\n2\r\n2\r\n2\r\n2\r\n2\r\n2\r\n2\r\n2";
+        }
+
+        /// <summary>
+        /// 文件复制
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void execute_Click(object sender, EventArgs e)
+        {
+            txtResult.Text = null;
+            lblResult.Text = null;
+
+            if (!CheckInput()) {
+                return;
+            }
+
+            string sourcePath = txtSource.Text;
+            string destinationPath = txtDestination.Text;
+            string filelist = txtFileList.Text;
+
+            string[] files = filelist.Split(new string[] {"\r\n", "\n"}, StringSplitOptions.None);
+
+            progressBar.Show();
+            progressBar.Maximum = files.Count();
+
+            //
+            int totalCount = 0;
+            int successedCount = 0;
+            int failedCount = 0;
+            foreach (string file in files) {
+
+                if (String.IsNullOrEmpty(file)) {
+                    txtResult.Text += "\r\n";
+                    continue;
+                }
+
+                progressBar.Value = ++totalCount;
+
+                string sourceFileName = sourcePath + file;
+                string destFileName = destinationPath + file;
+
+                // 目录不存在的情况下，创建目录
+                string targetPath = Path.GetDirectoryName(destFileName);
+                if (!System.IO.Directory.Exists(targetPath))
+                {
+                    System.IO.Directory.CreateDirectory(targetPath);
+                }
+
+                // 目标文件存在，先删除
+                if (File.Exists(destFileName)) {
+                    File.Delete(destFileName);
+                }
+
+                if (File.Exists(sourceFileName))
+                {
+                    // 复制文件
+                    File.Copy(sourceFileName, destFileName);
+                    successedCount++;
+                    txtResult.Text += String.Format("{0}  |   copied！\r\n", file);
+                }
+                else {
+                    failedCount++;
+                    txtResult.Text += String.Format("{0}  |   file not exsit！\r\n", file);
+                }
+                
+            }
+
+            lblResult.Text = string.Format("finished! successed {0} , failed : {1}", successedCount, failedCount);
+            if (failedCount > 0) {
+                lblResult.ForeColor = Color.Red;
+            }
+        }
+
+        /// <summary>
+        /// 执行前输入内容检查
+        /// </summary>
+        /// <returns></returns>
+        private bool CheckInput() {
+            string sourcePath = txtSource.Text;
+            string destinationPath = txtDestination.Text;
+            string filelist = txtFileList.Text;
+
+            if (string.IsNullOrEmpty(sourcePath)) {
+                txtSource.Focus();
+                MessageBox.Show(string.Format("Source is empty !"));
+                return false;
+            }
+
+            if (!Directory.Exists(sourcePath))
+            {
+                MessageBox.Show(string.Format("Source is not directory!"));
+                txtSource.Focus();
+                return false;
+            }
+
+            if (string.IsNullOrEmpty(destinationPath))
+            {
+                MessageBox.Show(string.Format("Destination is empty !"));
+                txtDestination.Focus();
+                return false;
+            }
+
+            if (!Directory.Exists(destinationPath))
+            {
+                MessageBox.Show(string.Format("Destination is not directory!"));
+                txtDestination.Focus();
+                return false;
+            }
+
+            if (string.IsNullOrEmpty(filelist)) {
+                MessageBox.Show(string.Format("FileList is not directory!"));
+                txtFileList.Focus();
+                return false;
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// 退出
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        /// <summary>
+        /// 文件夹选择
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void txtSource_DoubleClick(object sender, EventArgs e)
+        {
+            string path = SelectFolder("");
+            if (!string.IsNullOrEmpty(path)) {
+                txtSource.Text = path;
+                reset();
+            }
+        }
+
+        /// <summary>
+        /// 文件夹选择
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void txtDestination_DoubleClick(object sender, EventArgs e)
+        {
+
+            string path = SelectFolder("");
+            if (!string.IsNullOrEmpty(path))
+            {
+                txtDestination.Text = path;
+                reset();
+            }
+        }
+
+        private void reset() {
+            progressBar.Hide();
+            lblResult.Text = null;
+            txtResult.Text = null;
+        }
+
+        /// <summary>
+        /// 文件夹选择框
+        /// </summary>
+        /// <param name="title"></param>
+        /// <returns></returns>
+        private string SelectFolder(string title) {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+
+            fbd.Description = title;
+            fbd.RootFolder = Environment.SpecialFolder.Desktop;
+            fbd.SelectedPath = @"C:\";
+            fbd.ShowNewFolderButton = true;
+
+            if (fbd.ShowDialog(this) == DialogResult.OK)
+            {
+                return fbd.SelectedPath;
+            }
+
+            return null;
+        }
+
+    }
+}
